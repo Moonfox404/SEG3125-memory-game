ddcc0b698561f25a810794773d58526352aae15a
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2025-06-18 12:34:58 -0400

    Update Navbar

--- memory-game/app/components/NavBar.tsx
+++ memory-game/app/components/NavBar.tsx
@@ -1,27 +1,26 @@
-import { faCircleQuestion, faGear } from "@fortawesome/free-solid-svg-icons";
+import { faCircleQuestion, faSliders } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ThemeToggle from "./ThemeToggle";
 
 const NavBar = () => {
   return (
     <div>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base shadow-sm">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">Swapped!</a>
+          <a className="btn btn-ghost text-xl text-primary">Swapped!</a>
         </div>
-        <div className="flex-none">
+        <div className="flex-none text-primary">
           <ul className="menu menu-horizontal px-1">
             <li className="">
-              {/* replace with Theme toggle here */}
               <ThemeToggle />
             </li>
             <li>
-              <a>
-                <FontAwesomeIcon icon={faGear} />
+              <a className="h-full flex items-center">
+                <FontAwesomeIcon icon={faSliders} />
               </a>
             </li>
             <li>
-              <a href="">
+              <a className="h-full flex items-center">
                 {" "}
                 <FontAwesomeIcon icon={faCircleQuestion} />
               </a>
