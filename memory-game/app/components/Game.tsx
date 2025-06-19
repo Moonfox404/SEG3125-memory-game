88480ae25cdc95ca9e3f99296f9bef728550c09c
Xuan Pham <hpham024@uottawa.ca>
2025-06-19 11:51:29 -0400

    working flip check-match logic

--- memory-game/app/components/Game.tsx
+++ memory-game/app/components/Game.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef, useState } from "react";
 import useHighScore from "../hooks/useHighScore";
+import GameCard from "./GameCard";
 
 type GameCardModel = {
   state: "rest" | "flipped" | "matched";
@@ -12,13 +13,19 @@ type GameProps = {
   theme: "Fruit" | "Animal" | "Heart";
   boardSize: number;  // 0, 1, or 2 (for small, medium, large)
   swapsPerTurn: number;  // 0, 1, or 2
+  paused: boolean;
+  handleGameEnd: (score: number, highScore: number) => void;
 };
 
-const Game = ({ theme, boardSize, swapsPerTurn }: GameProps) => {
+const calculateScore = (time: number, moves: number, boardSize: number) => {
+  return (60 * 60 - time) * (boardSize / moves);
+}
+
+const Game = ({ theme, boardSize, swapsPerTurn, paused, handleGameEnd }: GameProps) => {
   // state from props
   const boardHeight = 2 + boardSize;
   const boardWidth = 4 + Number(boardSize === 2);
-  const boardValues = Array.from({ length: boardHeight * boardWidth / 2 + 1 }, (_, i) => i );
+  const boardValues = Array.from({ length: boardHeight * boardWidth / 2 }, (_, i) => i );
 
   // initialise state
   const [highScore, setHighScore] = useHighScore();
@@ -40,6 +47,9 @@ const Game = ({ theme, boardSize, swapsPerTurn }: GameProps) => {
   // functions for game logic
   const handleClick = (index: number) => {
     revealedCards.current.push(index);
+    const newGameModel = [...gameModel];
+    newGameModel[index].state = "flipped";
+    setGameModel(newGameModel);
     setMoves(moves + 1);
 
     if (revealedCards.current.length === 2) {
@@ -50,10 +60,15 @@ const Game = ({ theme, boardSize, swapsPerTurn }: GameProps) => {
 
   const checkMatch = async () => {
     const updateModel = (state: "matched" | "rest") => {
+      console.log("updating model");
+      console.log(gameModel);
+
       const updatedModel = [...gameModel];
       updatedModel[revealedCards.current[0]].state = state;
       updatedModel[revealedCards.current[1]].state = state;
       setGameModel(updatedModel);
+
+      console.log(updatedModel);
 
       // reset revealedCards
       revealedCards.current.length = 0;
@@ -64,12 +79,13 @@ const Game = ({ theme, boardSize, swapsPerTurn }: GameProps) => {
       const newTilesMatched = cardsMatched + 2;
       if (newTilesMatched === boardHeight * boardWidth) {
         // game won
+        handleGameCompletion();
       }
-      setCardsMatched(newTilesMatched);
 
       activeIndices.current.delete(revealedCards.current[0]);
       activeIndices.current.delete(revealedCards.current[1]);
-
+      
+      setCardsMatched(newTilesMatched);
       updateModel("matched");
 
     } else {
@@ -80,35 +96,61 @@ const Game = ({ theme, boardSize, swapsPerTurn }: GameProps) => {
     }
   };
 
-  const swapCards = (i: number) => {
-    const indexArray = Array.from(activeIndices.current);
-    const firstIndex = Math.floor(Math.random() * indexArray.length);
-    const secondIndex = Math.floor(Math.random() * (indexArray.length - 1));
+  const swapCards = () => {
+    if (swapNumber < swapsPerTurn) {
+      console.log("swapping cards");
+      console.log(gameModel);
 
-    const firstCard = indexArray[firstIndex];
-    const secondCard = [...indexArray.slice(0, firstIndex), ...indexArray.slice(firstIndex + 1, -1)][secondIndex];
+      const indexArray = Array.from(activeIndices.current);
+      const firstIndex = Math.floor(Math.random() * indexArray.length);
+      const secondIndex = Math.floor(Math.random() * (indexArray.length - 1));
 
-    const updatedModel = [...gameModel];
-    updatedModel[firstCard] = gameModel[secondCard];
-    updatedModel[secondCard] = gameModel[firstCard];
+      const firstCard = indexArray[firstIndex];
+      const secondCard = [...indexArray.slice(0, firstIndex), ...indexArray.slice(firstIndex + 1, -1)][secondIndex];
 
-    setTimeout(() => {
-      setGameModel(updatedModel);
+      const updatedModel = [...gameModel];
+      updatedModel[firstCard] = gameModel[secondCard];
+      updatedModel[secondCard] = gameModel[firstCard];
 
-      if (i === swapsPerTurn - 1) {
-        setTurn(true);
-      }
-    }, 1000 * i);
-  }
+      setTimeout(() => {
+        setGameModel(updatedModel);
+        setSwapNumber(swapNumber + 1);
+      }, 1000);
+    } else {
+      setTurn(true);
+    }
+  };
+
+  const handleGameCompletion = () => {
+    const score = calculateScore(time, moves, boardHeight * boardWidth);
+    if (!highScore || score > highScore) {
+      setHighScore(score);
+      handleGameEnd(score, score);
+    } else {
+      handleGameEnd(score, highScore);
+    }
+  };
 
   useEffect(() => {
     if (!turn) {
-      swapCards(swapNumber);
-      setSwapNumber(swapNumber + 1);
+      swapCards();
     } else {
       setSwapNumber(0);
     }
   }, [gameModel]);  // run swap every time game model is updated at end of user's turn
+
+  // tsx component
+  return (
+    <div className="grid grid-cols-4">
+      {
+        gameModel.map((model, idx) => {
+          return <div key={idx} className="row w-fit">
+            <GameCard index={idx} item={model.item} state={model.state} theme={theme} disabled={paused || !turn} onClick={handleClick}/>
+          </div>;
+        })
+      }
+    </div>
+  );
 };
 
 export default Game;
--- memory-game/app/components/GameCard.tsx
+++ memory-game/app/components/GameCard.tsx
@@ -1,20 +1,29 @@
 "use client"
 
+import { useState } from "react";
+
 // using colours to test game logic before implementing animations
 const colours = ["red", "blue", "green", "yellow", "purple", "aqua", "coral", "pink", "orchid", "aquamarine"];
 const backColour = "beige";
 
 type GameCardProps = {
   item: number;
+  index: number;
   state: "rest" | "flipped" | "matched";
   theme: "Fruit" | "Animal" | "Heart";
   disabled: boolean;
-  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  onClick: (index: number) => void;
 }
 
-const GameCard = ({ item, state, onClick }: GameCardProps) => {
+const GameCard = ({ item, index, state, theme, disabled, onClick }: GameCardProps) => {
+  const handleClick = () => {
+    if (!disabled) {
+      onClick(index);
+    }
+  }
+
   return (
-    <div className="card w-50 h-50" onClick={onClick} style={{background: state === "rest" ? backColour : colours[item]}}>
+    <div className="card w-50 h-50" onClick={() => {handleClick()}} style={{background: state === "rest" ? backColour : colours[item]}}>
     </div>
   );
 }
